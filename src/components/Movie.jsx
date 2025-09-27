add9f8ad09805120b9be387d0ec26e4a8442f285
Abhilash Madhu <abhilashmadhu649@gmail.com>
2025-09-27 21:13:42 +0530

    Added pagination

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,30 +1,55 @@
-import React, { useEffect, useState } from 'react'
-import MovieCard from './MovieCard'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import MovieCard from "./MovieCard";
+import axios from "axios";
+import Pagination from "./Pagination";
 
 function Movie() {
-     const [Movies , setMovies] = useState([])
-     useEffect(()=>{
-    axios.get('https://api.themoviedb.org/3/movie/popular?api_key=b4ffaae9e110930fad67bcb67972ac26&language=en-US&page=1').then(function(res){
-      setMovies(res.data.results)
-    })
+  const [Movies, setMovies] = useState([]);
+  let [pageNo,setPageNo] = useState(1);
 
-   },[])
+  const handlePrev = ()=>{
+    if(pageNo==1){
+    setPageNo=1
+    }
+    else{
+      setPageNo(pageNo-1)
+    }
+    
+    
+    
+  }
+
+  const handleNext = ()=>{
+    setPageNo(pageNo+1)
+  }
+
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/popular?api_key=b4ffaae9e110930fad67bcb67972ac26&language=en-US&page=${pageNo}`
+      )
+      .then(function (res) {
+        setMovies(res.data.results);
+      });
+  }, [pageNo]);
   return (
     <div>
-      <div className='text-xl font-bold text-center'>
-        Trending Movies
-      </div>
-      <div className='p-3 flex flex-row justify-around flex-wrap'>
-        {Movies.map((movieObj)=>{
-          return(
-          <MovieCard key={movieObj.id} poster_path={movieObj.poster_path} name={movieObj.original_title}/>
-          )
+      <div className="text-xl font-bold text-center">Trending Movies</div>
+      <div className="p-3 flex flex-row justify-around flex-wrap">
+        {Movies.map((movieObj) => {
+          return (
+            <MovieCard
+              key={movieObj.id}
+              poster_path={movieObj.poster_path}
+              name={movieObj.original_title}
+            />
+          );
         })}
-   
       </div>
+      <Pagination pageNo={pageNo} handleNext={handleNext} handlePrev={handlePrev}/>
     </div>
-  )
+  );
 }
 
-export default Movie
+export default Movie;
