503942299b06120137e6e8d48ea70d0dc2608c64
Abhilash Madhu <abhilashmadhu649@gmail.com>
2025-09-27 13:13:30 +0530

    Added the movies list using TMDB Api

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,26 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MovieCard from './MovieCard'
+import axios from 'axios'
 
 function Movie() {
+     const [Movies , setMovies] = useState([])
+     useEffect(()=>{
+    axios.get('https://api.themoviedb.org/3/movie/popular?api_key=b4ffaae9e110930fad67bcb67972ac26&language=en-US&page=1').then(function(res){
+      setMovies(res.data.results)
+    })
+
+   },[])
   return (
     <div>
       <div className='text-xl font-bold text-center'>
-        Trendiing Movies
+        Trending Movies
       </div>
       <div className='p-3 flex flex-row justify-around flex-wrap'>
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
+        {Movies.map((movieObj)=>{
+          return(
+          <MovieCard key={movieObj.id} poster_path={movieObj.poster_path} name={movieObj.original_title}/>
+          )
+        })}
+   
       </div>
     </div>
   )
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
-function MovieCard() {
+function MovieCard({ poster_path , name}) {  
+  
   return (
-    <div className='h-[40vh] w-[200px] bg-center bg-cover rounded-xl hover:cursor-pointer hover:scale-110 duration-300 flex flex-col justify-between items-end m-2' style={{backgroundImage : `url('https://i0.wp.com/www.shutterstock.com/blog/wp-content/uploads/sites/5/2024/03/Stopmotion-poster.jpg?ssl=1')`}}>
-    
+    <div
+      className="h-[40vh] w-[200px] bg-center bg-cover rounded-xl hover:cursor-pointer hover:scale-110 duration-300 flex flex-col justify-between items-end m-2"
+      style={{
+        backgroundImage: `url(https://image.tmdb.org/t/p/original/${poster_path})`,
+      }}
+    >
+      <div className="text-white text-xl w-full p-2  text-center">
+        {name}
+      </div>
     </div>
-  )
+  );
 }
 
-export default MovieCard
+export default MovieCard;
